ae77f9d8a63c82bb453afe2521d708231a9a163c
= <=>
2019-05-28 18:24:55 +0200

    Created Turing class

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Maszyna Turinga
+// Autor: Dominik Kedzia
+// WSZiB w Krakowie
+
+#include <iostream>
+
+#include "turing.class.cpp"
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	Turing maszyna1;
+	maszyna1.writeInfo();
+}
---
+++ turing.class.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Turing{
+	public:
+		int id;
+		
+		void writeInfo()
+		{
+			std::cout << "Maszyna Turinga" << std::endl;
+		}
+};
